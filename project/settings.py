609e637bd33eaeab7499df36891f7897ca996f30
Muqrrab <muqrrab2@gmail.com>
2023-06-14 15:37:48 +0500

    main

--- project/settings.py
+++ project/settings.py
@@ -133,7 +133,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
     'http://localhost:3000',
-    'http://127.0.0.1:9000'
+    'http://127.0.0.1:9000',
+    'http://142.93.255.174:3000',
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
