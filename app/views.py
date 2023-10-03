a31262afc0222aa62dabddc3666501182de84c99
itsabubakarshf <ItsAbubakarShf@gmail.com>
2023-10-03 23:18:58 +0500

    completed

--- app/views.py
+++ app/views.py
@@ -29,67 +29,71 @@ def preprocess_image(image):
 import base64
 
 
+from PIL import Image
+
 @csrf_exempt
 def predict(request):
-    if request.method == 'POST':
-        if request.FILES.get('file'):
-            # Get the image from the incoming POST request
-            # image_file = request.FILES['file']
-            # data = request.data
-            # kind = data['kind']
+    try:
+        if request.method == 'POST':
+            if request.FILES.get('file'):
+                print("Received an image for prediction")
+                
+                image_file = request.FILES.get('file')
+                file_name = 'temp.jpg'
+                file_path = os.path.join(settings.STATIC_ROOT, file_name)
 
-            # if kind == 'upload':
-            image_file = request.FILES.get('file')  # Access the uploaded file
-            # kind = request.POST.get('kind')  # Access the 'kind' field
-            file_name = 'temp.jpg'
-            file_path = os.path.join(settings.STATIC_ROOT, file_name)
+                # Check if the image is not a JPG and convert if needed
+                im = Image.open(image_file)
+                if im.format != "JPEG":
+                    print(f"Image format is {im.format}. Converting to JPG.")
+                    im = im.convert('RGB')  # Ensure image has 3 channels
+                    im.save(file_path, "JPEG")
+                else:
+                    with open(file_path, 'wb') as f:
+                        for chunk in image_file.chunks():
+                            f.write(chunk)
+                
+                print("Saved image to:", file_path)
+
+                image = Image.open(file_path)
+                image = preprocess_image(image)
+                print("Preprocessed the image")
+
+                prediction = model.predict(image)
+                print("Prediction completed")
+                
+                predicted_class_indices = np.argmax(prediction, axis=1)
+                index = ['0.5', 'Healthy', '0.10', 'Healthy', '0.52', 'Healthy', '0.65', 'Healthy', '0.89', 'Healthy', '0.99', 'Healthy', '1.2', 'Healthy', '1.5', 'Healthy', '1.66', 'Healthy', '1.76', 'Healthy', '2', 'Healthy']
+                
+                sorted_indices = np.argsort(prediction[0])[::-1]
+                new_index = index[sorted_indices[predicted_class_indices[0]]]
+                
+                healthy_or_deficiency = 'Healthy'
+                deficiency_percent = 0.0
+                if new_index != 'Healthy':
+                    healthy_or_deficiency = 'Deficiency'
+                    deficiency_percent = new_index
+
+                os.remove(file_path)
+                print("Temp image file removed")
+
+                response = {
+                    'healthy_or_deficiency': healthy_or_deficiency,
+                    'deficiency_percent': float(deficiency_percent),
+                    'file_path': file_path
+                }
+
+                print("Sending response:", response)
+                return JsonResponse(response, safe=False)
             
-            with open(file_path, 'wb') as f:
-                for chunk in image_file.chunks():
-                    f.write(chunk)
-
-            # else:
-            #     print('camera inside')
-            #     format, imgstr = image_file.split(';base64,') 
-            #     ext = format.split('/')[-1] 
-            #     data = base64.b64decode(imgstr)
-
-            #     filename = os.path.join(settings.STATIC_ROOT, 'temp.' + ext)
-            #     with open(filename, 'wb') as f:
-            #         f.write(data)
-
-            # Open the image file
-            image = Image.open(file_path)
-            # Preprocess the image
-            image = preprocess_image(image)
-            # Make a prediction
-            prediction = model.predict(image)
-
-
-            predicted_class_indices = np.argmax(prediction, axis=1)
-
-            index = ['0.5', 'Healthy', '0.10', 'Healthy','0.52', 'Healthy','0.65', 'Healthy', '0.89', 'Healthy','0.99', 'Healthy','1.2', 'Healthy','1.5', 'Healthy','1.66', 'Healthy','1.76', 'Healthy','2', 'Healthy' ]
-
-            sorted_indices = np.argsort(prediction[0])[::-1] 
-
-            new_index = index[sorted_indices[predicted_class_indices[0]]]
-            healthy_or_deficiency = 'Healthy'
-            deficiency_percent = 0.0
-            if new_index != 'Healthy':
-                healthy_or_deficiency = 'Deficiency'
-                deficiency_percent = new_index
-
-            os.remove(file_path)
-
-            response = {
-                'healthy_or_deficiency': healthy_or_deficiency,
-                'deficiency_percent': float(deficiency_percent),
-                'file_path': file_path
-            }
-
-            return JsonResponse(response, safe=False)
+            else:
+                print("Error: No image file provided")
+                return JsonResponse({'error': 'No image file provided'}, status=400)
         
         else:
-            return JsonResponse({'error': 'No image file provided'}, status=400)
-    else:
-        return JsonResponse({'error': 'Invalid request method'}, status=400)
+            print("Error: Invalid request method")
+            return JsonResponse({'error': 'Invalid request method'}, status=400)
+    
+    except Exception as e:
+        print("Exception encountered:", e)
+        return JsonResponse({'error': 'Server Error'}, status=500)
